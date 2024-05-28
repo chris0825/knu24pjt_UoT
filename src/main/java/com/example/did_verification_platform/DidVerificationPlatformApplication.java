72a913ad7e4d9b0111a550a68d051fb556beeb22
DH <rainyday1367@naver.com>
2024-05-28 22:17:24 +0900

    feat: Creat basecode

---
+++ src/main/java/com/example/did_verification_platform/DidVerificationPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.example.did_verification_platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DidVerificationPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DidVerificationPlatformApplication.class, args);
+	}
+
+}
